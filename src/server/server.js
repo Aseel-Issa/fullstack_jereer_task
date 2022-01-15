f742923f1aed1a3b6353e1718fd04ae790b7cb30
aseel <aseelissa91@hotmail.com>
2022-01-16 00:05:18 +0200

    updating server.js to serve client files

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const api = require('./api')
 const app = express()
 const path = require('path')
+const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -12,13 +13,19 @@ app.use(function (req, res, next) {
 
     next()
 })
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(publicPath));
 
 app.use('/', api)
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // server code is runnable on port 3001
 const port = process.env.PORT || 3001
