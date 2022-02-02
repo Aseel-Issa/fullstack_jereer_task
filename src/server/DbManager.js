2c876b653cfe013923fc40885811230a2ef9521b
aseel <aseelissa91@hotmail.com>
2022-02-02 17:27:18 +0200

    delete unnessesary comments

--- src/server/DbManager.js
+++ src/server/DbManager.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 // mongoose is a singleton, so connecting to db once is enough
-
-// mongoose.connect('mongodb+srv://admin:mongoPass391@jereer-db.dqjdn.mongodb.net/JEREER-TASK?retryWrites=true&w=majority', { useNewUrlParser: true })
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/JEREER-TASK', { useNewUrlParser: true })
 const User = require('./dbModels/User')
 const Post = require('./dbModels/Post')
